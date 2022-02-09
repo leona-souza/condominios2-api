595334a8e049690c13254c9b98bb588c24fed2d6
Leona Souza <leonasouza.dev@gmail.com>
2022-02-08 22:14:04 -0300

    feat: visitantes endpoints working

--- src/visitante/visitante.controller.ts
+++ src/visitante/visitante.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Visitante } from 'src/visitante/models/visitante.interface';
+import { VisitanteService } from 'src/visitante/visitante.service';
 
 @Controller('visitante')
-export class VisitanteController {}
+export class VisitanteController {
+  constructor(private visitanteService: VisitanteService) {}
+
+  @Get()
+  findAll() {
+    return this.visitanteService.findAll()
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: number) {
+    return this.visitanteService.findById(id)
+  }
+
+  @Get('/all')
+  findAllWithRelations() {
+    return this.visitanteService.findAllWithRelations()
+  }
+
+  @Post()
+  create(@Body() visitante: Visitante) {
+    return this.visitanteService.createVisitante(visitante)
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() visitante: Visitante
+  ) {
+    return this.visitanteService.updateVisitante(id, visitante)
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.visitanteService.deleteVisitante(id)
+  }
+}
--- src/visitante/visitante.service.ts
+++ src/visitante/visitante.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { VisitanteEntity } from 'src/visitante/models/visitante.entity';
+import { Visitante } from 'src/visitante/models/visitante.interface';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class VisitanteService {}
+export class VisitanteService {
+  constructor(
+    @InjectRepository(VisitanteEntity)
+    private readonly visitanteRepository: Repository<VisitanteEntity>
+  ) {}
+
+  findAll() {
+    return this.visitanteRepository.find()
+  }
+
+  findAllWithRelations() {
+    return this.visitanteRepository.find({
+      relations: ['apartamento'],
+    })
+  }
+
+  findById(id: number) {
+    return this.visitanteRepository.findOne({
+      where: { id: id },
+      relations: ['apartamento'],
+    })
+  }
+
+  createVisitante(visitante: Visitante) {
+    return this.visitanteRepository.save(visitante)
+  }
+
+  updateVisitante(id: number, visitante: Visitante) {
+    return this.visitanteRepository.update(id, visitante)
+  }
+
+  deleteVisitante(id: number) {
+    return this.visitanteRepository.delete(id)
+  }
+}
