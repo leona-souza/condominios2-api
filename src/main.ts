0e32a9e0471e9c79c5531dbc79d9466b68609335
Leona Souza <leonasouza.dev@gmail.com>
2022-04-25 22:06:50 -0300

    chore: cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
+<<<<<<< Updated upstream
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(5000);
+=======
+  const app = await NestFactory.create(AppModule, {cors: true})
+  app.enableCors()
+  await app.listen(5000)
+>>>>>>> Stashed changes
 }
-bootstrap();
+bootstrap()
